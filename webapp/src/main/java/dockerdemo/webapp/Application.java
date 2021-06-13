8b0c939a482c9e1c03d0fb7a0e59e9639c8442d8
Johan Sandberg <johan@smood.com>
2021-06-13 08:13:32 +0200

    Konfigurerar component-scan utanför egna modulen

--- webapp/src/main/java/dockerdemo/webapp/Application.java
+++ webapp/src/main/java/dockerdemo/webapp/Application.java
@@ -1,13 +1,12 @@
-package dockerdemo.app;
+package dockerdemo.webapp;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = dockerdemo.app.Application.class)
 public class Application extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
