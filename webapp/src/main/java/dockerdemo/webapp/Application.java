ae40d028cb8aea902bfee60f850b5a4ade565ade
Johan Sandberg <johan@smood.com>
2021-06-13 08:25:22 +0200

    Lägger tillbaka psvm

--- webapp/src/main/java/dockerdemo/webapp/Application.java
+++ webapp/src/main/java/dockerdemo/webapp/Application.java
@@ -9,4 +9,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication(scanBasePackageClasses = dockerdemo.app.Application.class)
 public class Application extends SpringBootServletInitializer
 {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
 }
