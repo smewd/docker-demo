05c037f300c7631078204076863d97e0344e4167
Johan Sandberg <johan@smood.com>
2021-06-13 08:14:45 +0200

    Renodlar

--- webapp/src/main/java/dockerdemo/webapp/Application.java
+++ webapp/src/main/java/dockerdemo/webapp/Application.java
@@ -9,8 +9,4 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication(scanBasePackageClasses = dockerdemo.app.Application.class)
 public class Application extends SpringBootServletInitializer
 {
-	public static void main(String[] args)
-	{
-		SpringApplication.run(Application.class, args);
-	}
 }
